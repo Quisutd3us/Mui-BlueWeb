d76c7a3e8e5f3025cb46565693f8981746539ea0
d3us <dnarino@gmail.com>
2023-09-27 16:34:58 -0500

    box

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,36 @@
-import { Button, Container, Typography } from "@mui/material";
+import { Box, Button, Container, Typography } from "@mui/material";
 
 export const App = () => {
   return (
-    <Container maxWidth={"sm"} sx={{ boxShadow: 4, pb: 2, m: 1 }}>
-      <h1>Hi World</h1>
-      <Typography variant="h2">App H1 con componente</Typography>
-      <Typography
-        my={2}
-        textAlign={'center'}
-        component={'div'}
-        variant="body1">App H1 con componente</Typography>
-      <Button variant="contained">MY first Buttom</Button>
-    </Container>
+    <>
+      <Container maxWidth={"sm"} sx={{ boxShadow: 4, pb: 2, m: 1 }}>
+        <h1>Hi World</h1>
+        <Typography variant="h2">App H1 con componente</Typography>
+        <Typography
+          my={2}
+          textAlign={'center'}
+          component={'div'}
+          variant="body1">App H1 con componente</Typography>
+        <Button variant="contained">MY first Buttom</Button>
+      </Container>
+      <Container maxWidth={"sm"} sx={{ boxShadow: 4, pb: 2, m: 1 }}>
+        <Typography
+          variant="h3"
+        >Box</Typography>
+        <Box>
+          <Typography
+            sx={{
+              border: 2,
+              my:3,
+              p: 5,
+              borderColor: "peru",
+              borderRadius:2,
+              bgcolor: "#333",
+              color: 'white'
+            }}
+            variant="body1">Que hermoso es MUI</Typography>
+        </Box>
+      </Container>
+    </>
   );
 };
